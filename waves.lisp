cf07dabdbc71d551aa48f42ab4003da215098ffb
gingeralesy <gingeralesy@gmail.com>
2017-04-19 19:08:07 +0300

    Teeny tiny optimisation

--- waves.lisp
+++ waves.lisp
@@ -8,15 +8,15 @@
                                                             :encoding :float))))
 
 (defun sine-wave (position frequency sample-rate)
-  (let ((wave-length (floor (/ sample-rate frequency))))
+  (let ((wave-length (floor sample-rate frequency)))
     (sin (* (/ (mod position wave-length) wave-length) (coerce pi 'single-float) 2))))
 
 (defun square-wave (position frequency sample-rate)
-  (let ((wave-length (floor (/ sample-rate frequency))))
+  (let ((wave-length (floor sample-rate frequency)))
     (if (< (mod position wave-length) (/ wave-length 2)) 1.0s0 -1.0s0)))
 
 (defun triangle-wave (position frequency sample-rate)
-  (let* ((wave-length (floor (/ sample-rate frequency)))
+  (let* ((wave-length (floor sample-rate frequency))
          (quarter-wave (/ wave-length 4.0s0))
          (position (mod position wave-length))
          (multiplier (/ 4.0s0 wave-length)))
@@ -26,7 +26,7 @@
       (T (- 1.0s0 (* (- position quarter-wave) multiplier))))))
 
 (defun sawtooth-wave (position frequency sample-rate)
-  (let* ((wave-length (floor (/ sample-rate frequency)))
+  (let* ((wave-length (floor sample-rate frequency))
          (value (/ (mod position wave-length) (* 2.0s0 wave-length))))
     (if (< 1.0s0 value) (- value 2.0s0) value)))
 
